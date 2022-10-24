f2b2fb88c2e03fc1f0cb72f2e9cec5403c6330c4
Luckas <luckasranarison@gmail.com>
2022-10-24 11:54:12 +0300

    improved moveKnight()

--- js/moves.js
+++ js/moves.js
@@ -2,6 +2,7 @@ function movePawn() {
     // moves
     currentPiece.y === player.frontLine ? (max = 2) : (max = 1);
 
+    // pawn move differently for the two players, that's why each player have his own method
     for (let i = 1; i <= max; i++) {
         let temp = atIndex(player.operation(currentPiece.y, i), currentPiece.x);
 
@@ -36,53 +37,36 @@ function moveRook() {
 }
 
 function moveKnight() {
-    let temp = [];
-    let j, k, l, m;
-    let coord1 = currentPiece.x;
-    let coord2 = currentPiece.y;
-    let operation = increment;
-    let operationMirror = decrement;
+    // used antiLoop() to preventn multiple negative values
+    let j = antiLoop(currentPiece.x + 2);
+    let k = antiLoop(currentPiece.x - 2);
+    let l = antiLoop(currentPiece.y + 1);
+    let m = antiLoop(currentPiece.y - 1);
 
-    // loop twice to get all 8 possibles cases
-    for (let i = 0; i < 2; i++) {
-        j = operation(coord1, 2);
-        k = operationMirror(coord1, 2);
-        l = operation(coord2);
-        m = operationMirror(coord2);
+    // figured all cases instead of doing a loop
+    temp = [
+        atIndex(l, j),
+        atIndex(m, j),
+        atIndex(l, k),
+        atIndex(m, k),
+        atIndex(l + 1, k + 1),
+        atIndex(l + 1, j - 1),
+        atIndex(m - 1, k + 1),
+        atIndex(m - 1, j - 1),
+    ];
 
-        // verify if the number is positive to prevent loop
-        if (i === 0) {
-            if (l > -1 && m > -1) {
-                if (j > -1) temp.push(atIndex(l, j), atIndex(m, j));
-                if (k > -1) temp.push(atIndex(l, k), atIndex(m, k));
-            }
-        } else {
-            // the second iteration uses the opposite coordinates of the first one
-            if (l > -1 && m > -1) {
-                if (j > -1) temp.push(atIndex(j, l), atIndex(j, m));
-                if (k > -1) temp.push(atIndex(k, l), atIndex(k, m));
-            }
+    for (const t of temp) {
+        // capture
+        if (t.hasClass(opponent.name) && !possibleCaptures.includes(t)) {
+            possibleCaptures.push(t);
         }
 
-        for (const t of temp) {
-            // capture
-            if (t.hasClass(opponent.name) && !possibleCaptures.includes(t)) {
-                possibleCaptures.push(t);
-            }
-
-            // move
-            if (!t.hasClass(player.name) && t.length !== 0) {
-                if (!possibleMoves.includes(t) && !possibleCaptures.includes(t))
-                    // to avoid duplication
-                    possibleMoves.push(t);
-            }
+        // move
+        if (!t.hasClass(player.name) && t.length !== 0) {
+            if (!possibleMoves.includes(t) && !possibleCaptures.includes(t))
+                // to avoid duplication
+                possibleMoves.push(t);
         }
-
-        // swap values
-        coord1 = currentPiece.y;
-        coord2 = currentPiece.x;
-        operation = decrement;
-        operationMirror = increment;
     }
 }
 
@@ -105,36 +89,32 @@ function moveKing() {
 
 function linearMove(direction) {
     //initialisation
-    let j, k;
     let operation = increment;
     let operationMirror = decrement;
 
     for (let i = 0; i < 2; i++) {
         switch (direction) {
             case "vertical":
-                j = operation(currentPiece.y);
+                j = antiLoop(operation(currentPiece.y));
                 temp = atIndex(j, currentPiece.x);
                 break;
             case "horizontal":
-                j = operation(currentPiece.x);
+                j = antiLoop(operation(currentPiece.x));
                 temp = atIndex(currentPiece.y, j);
                 break;
             case "diagonal1":
-                j = operation(currentPiece.y);
-                k = operation(currentPiece.x);
+                j = antiLoop(operation(currentPiece.y));
+                k = antiLoop(operation(currentPiece.x));
                 temp = atIndex(j, k);
                 break;
             case "diagonal2":
-                j = operation(currentPiece.y);
-                k = operationMirror(currentPiece.x);
+                j = antiLoop(operation(currentPiece.y));
+                k = antiLoop(operationMirror(currentPiece.x));
                 temp = atIndex(j, k);
                 break;
         }
 
         while (!temp.hasClass(player.name) && temp.length !== 0) {
-            //prevent query loop
-            if (j < 0 || k < 0) break;
-
             // capture
             if (temp.hasClass(opponent.name)) {
                 possibleCaptures.push(temp);
@@ -146,21 +126,21 @@ function linearMove(direction) {
 
             switch (direction) {
                 case "vertical":
-                    j = operation(j);
+                    j = antiLoop(operation(j));
                     temp = atIndex(j, currentPiece.x);
                     break;
                 case "horizontal":
-                    j = operation(j);
+                    j = antiLoop(operation(j));
                     temp = atIndex(currentPiece.y, j);
                     break;
                 case "diagonal1":
-                    j = operation(j);
-                    k = operation(k);
+                    j = antiLoop(operation(j));
+                    k = antiLoop(operation(k));
                     temp = atIndex(j, k);
                     break;
                 case "diagonal2":
-                    j = operation(j);
-                    k = operationMirror(k);
+                    j = antiLoop(operation(j));
+                    k = antiLoop(operationMirror(k));
                     temp = atIndex(j, k);
                     break;
             }
@@ -169,6 +149,13 @@ function linearMove(direction) {
         operation = decrement;
         operationMirror = increment;
     }
+}
+
+function antiLoop(n) {
+    // prevent negative numbers
+    if (n < 0) {
+        return undefined;
+    } else return n;
 }
 
 const MOVES = {
